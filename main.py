464a8263ac5aa259390a2ea7f9fedd9a91cf0129
neafanasev <neafanasev@gmail.com>
2021-10-09 15:31:18 +0300

    HTML и CSS болванка для теста экспорта БД

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 def print_hi(name):
-
     print(f'Hi, {name}')
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route("/", methods=["POST", "GET"])
+def process():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
